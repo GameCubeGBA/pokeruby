5ca3e17631dad6c83a4733727b35299ad8e678f7
Wiz <95232096+GameCubeGBA@users.noreply.github.com>
2022-08-06 12:40:45 -0400

    Match ClipRight and UpdateWindowText

--- src/text.c
+++ src/text.c
@@ -2406,6 +2406,8 @@ static u8 UpdateWindowText(struct Window *win)
 {
     switch (win->state)
     {
+    case WIN_STATE_END:
+        return TRUE; // done printing text
     case WIN_STATE_WAIT_BUTTON:
         if (PlayerCanInterruptDelay(win))
         {
@@ -2418,10 +2420,8 @@ static u8 UpdateWindowText(struct Window *win)
                 return FALSE;
             }
         }
-        else
+        else if (--win->delayCounter)
         {
-            win->delayCounter--;
-            if (win->delayCounter)
                 return FALSE;
         }
         win->state = WIN_STATE_NORMAL;
@@ -2436,14 +2436,12 @@ static u8 UpdateWindowText(struct Window *win)
             win->state = WIN_STATE_NORMAL;
             break;
         }
-        // fall through
+                // fall through
     case WIN_STATE_PAUSE:
         // Wait for timer to expire, then continue printing
-        if (win->delayCounter)
+        if (win->delayCounter && --win->delayCounter)
         {
-            win->delayCounter--;
-            if (win->delayCounter)
-                return FALSE;
+            return FALSE;
         }
         win->state = WIN_STATE_NORMAL;
         break;
@@ -2453,7 +2451,6 @@ static u8 UpdateWindowText(struct Window *win)
         {
             Text_ClearWindow(win);
             win->state = WIN_STATE_NORMAL;
-            asm("");
         }
         return FALSE;
     case WIN_STATE_WAIT_SCROLL:
@@ -2462,7 +2459,6 @@ static u8 UpdateWindowText(struct Window *win)
         {
             ScrollWindowTextLines(win);
             win->state = WIN_STATE_NORMAL;
-            asm("");
         }
         return FALSE;
     case WIN_STATE_PLACEHOLDER:
@@ -2472,7 +2468,6 @@ static u8 UpdateWindowText(struct Window *win)
     case WIN_STATE_NEWLINE:
         ScrollWindowTextLines(win);
         win->state = WIN_STATE_NORMAL;
-        asm("");
         return FALSE;
     case WIN_STATE_BEGIN:
         Text_ClearWindow(win);
@@ -2482,8 +2477,6 @@ static u8 UpdateWindowText(struct Window *win)
             return FALSE;
         win->state = WIN_STATE_NORMAL;
         break;
-    case WIN_STATE_END:
-        return TRUE;  // done printing text
     case WIN_STATE_NORMAL:
         break;
     default:
@@ -2495,25 +2488,25 @@ static u8 UpdateWindowText(struct Window *win)
 
     switch (win->state)
     {
-    case WIN_STATE_END:
-        return TRUE;  // done printing text
+    case WIN_STATE_PAUSE:
+    case WIN_STATE_NEWLINE:
+    case WIN_STATE_WAIT_SOUND:
+        return FALSE;
     case WIN_STATE_WAIT_BUTTON:
     case WIN_STATE_WAIT_CLEAR:
     case WIN_STATE_WAIT_SCROLL:
         if (PlayerCanInterruptDelay(win))
-            return 0;
+            return FALSE;
         win->delayCounter = 60;
-        break;
-    case WIN_STATE_PAUSE:
-    case WIN_STATE_NEWLINE:
-    case WIN_STATE_WAIT_SOUND:
-        break;
+        return FALSE;
+    case WIN_STATE_END:
+        return TRUE;  // done printing text
     default:
-        win->state = WIN_STATE_CHAR_DELAY;
-        win->delayCounter = GetTextDelay(win);
         break;
     }
 
+    win->state = WIN_STATE_CHAR_DELAY;
+    win->delayCounter = GetTextDelay(win);
     return 0;
 }
 
@@ -2839,12 +2832,12 @@ static void EraseAtCursor(struct Window *win)
 
 static void ClipLeft(struct Window *win)
 {
-    u32 pixel = win->left & 7;
+    u8 pixel = win->left & 7;
 
-    if (win->textMode != TEXT_MODE_MONOSPACE && pixel)
+    if (win->textMode != TEXT_MODE_MONOSPACE && pixel != 0)
     {
         const u32 *masks = sGlyphMasks[8][pixel];
-        u32 outsideMask = masks[0];
+        u32 outsideMask = *masks;
         u32 insideMask = ~outsideMask;
         u32 outside = sGlyphBuffer.background & outsideMask;
         u16 tileNum = GetCursorTileNum(win, 0, 0);
@@ -2872,15 +2865,14 @@ static void ClipLeft(struct Window *win)
 
 static void ClipRight(struct Window *win)
 {
-    register u8 cursorX asm("r0") = win->cursorX;
-    u8 left = win->left;
-    u32 pixelX = (cursorX + left) & 7;
+    u8 pixelX = (win->left + win->cursorX) & 7;
 
     if (win->textMode != TEXT_MODE_MONOSPACE && pixelX != 0)
     {
         const u32 *masks = sGlyphMasks[8 - pixelX][pixelX];
-        u32 insideMask = masks[0];
-        u32 outside = (sGlyphBuffer.background & ~insideMask);
+        u32 insideMask = *masks;
+        u32 outsideMask = ~insideMask;
+        u32 outside = sGlyphBuffer.background & outsideMask;
         u16 tileNum; 
         u32 *tileData;
 
@@ -2967,7 +2959,7 @@ static bool8 PlayerCanInterruptDelay(struct Window *win)
         retVal = FALSE;
         break;
     case WAIT_TYPE_CONTEST:
-        retVal = gIsLinkContest ? FALSE : TRUE;
+        retVal = !gIsLinkContest;
         break;
     case WAIT_TYPE_BATTLE:
         retVal = (gBattleTypeFlags & BATTLE_TYPE_LINK) ? FALSE : TRUE;
@@ -3741,12 +3733,9 @@ void ConvertInternationalString(u8 *s, u8 language)
         s[i++] = 22;
         s[i++] = EOS;
 
-        i--;
-
-        while (i != (u8)-1)
+        while (i--)
         {
             s[i + 2] = s[i];
-            i--;
         }
 
         s[0] = EXT_CTRL_CODE_BEGIN;
